e2d40103f7378dbfd9160d565973f714c80bbcea
John Paul <28johnpaulsim28@gmail.com>
2021-03-10 11:34:50 +0800

    add middleware

--- app/Http/Controllers/BorrowedController.php
+++ app/Http/Controllers/BorrowedController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class BorrowedController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:web');
+    }
     /**
      * Display a listing of the resource.
      *
